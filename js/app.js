d72faa8057167e7842c443b3e1e8ee898942998b
Marko Nikolovski <mz.nikolovski@gmail.com>
2014-04-08 15:55:46 -0700

    added posts table and links to individual posts

--- js/app.js
+++ js/app.js
@@ -3,4 +3,27 @@ App = Ember.Application.create();
 App.Router.map(function() {
   this.resource('about');
   this.resource('posts');
-})
+  this.resource('post', { path: ':post_id' });
+});
+
+App.PostsRoute = Ember.Route.extend({
+  model: function() {
+    return posts;
+  }
+});
+
+var posts = [{
+  id: '1',
+  title: "Rails is Omakase",
+  author: { name: "d2h" },
+  date: new Date('12-27-2012'),
+  excerpt: "Things things things and a couple of more things that are interesting",
+  body: "Dum doo doo dee, Ember is interesting for me"
+}, {
+  id: '2',
+  title: "The Parley Letter",
+  author: { name: "d2h" },
+  date: new Date('12-29-2012'),
+  excerpt: "Dumblebum a fooley-foo",
+  body: "Hamana hamana hamana haaaaaaaaa....aaa...a."
+}];

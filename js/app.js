9aef6062557d6d86ee62a16a60e5570220fbc4c8
Marko Nikolovski <mz.nikolovski@gmail.com>
2014-04-09 08:48:37 -0700

    added post controller, updated routers, included date helper

--- js/app.js
+++ js/app.js
@@ -2,14 +2,39 @@ App = Ember.Application.create();
 
 App.Router.map(function() {
   this.resource('about');
-  this.resource('posts');
-  this.resource('post', { path: ':post_id' });
+  this.resource('posts', function() {
+    this.resource('post', { path: ':post_id' });
+  });
 });
 
 App.PostsRoute = Ember.Route.extend({
   model: function() {
     return posts;
   }
+});
+
+App.PostRoute = Ember.Route.extend({
+  model: function(params) {
+      return posts.findBy('id', params.post_id);
+  }
+});
+
+App.PostController = Ember.ObjectController.extend ({
+  isEditing: false,
+
+  actions: {
+    edit: function() {
+      this.set('isEditing', true);
+    },
+
+    doneEditing: function() {
+      this.set('isEditing', false);
+    }
+  }
+});
+
+Ember.Handlebars.helper('format-date', function(date) {
+  return moment(date).fromNow();
 });
 
 var posts = [{
